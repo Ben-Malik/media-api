6db56c9fdc4a5b145c3c1fd5c721cc14fcef7210
Ben-Malik <benmaliktchamalam@gmail.com>
2022-03-02 02:39:55 +0300

    Added configs for deployment

--- src/main/java/com/my/media/MyMediaApplication.java
+++ src/main/java/com/my/media/MyMediaApplication.java
@@ -3,9 +3,11 @@ package com.my.media;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
 @ComponentScan( {"utility", "com.my.media"})
+@PropertySource("classpath:application-${spring.profiles.active:default}.properties")
 public class MyMediaApplication {
 
     public static void main(String[] args) {
