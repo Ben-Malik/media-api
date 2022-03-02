34818286498e942abc790453553ace9d7f1f875a
Ben-Malik <benmaliktchamalam@gmail.com>
2022-03-02 03:06:58 +0300

    Added config for deployment to heroku

--- src/main/java/com/my/media/MyMediaApplication.java
+++ src/main/java/com/my/media/MyMediaApplication.java
@@ -3,11 +3,9 @@ package com.my.media;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
 @ComponentScan( {"utility", "com.my.media"})
-@PropertySource("classpath:application-${spring.profiles.active:default}.properties")
 public class MyMediaApplication {
 
     public static void main(String[] args) {
