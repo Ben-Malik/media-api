ffddcf1e64df1e691b0d8030c147300517932ee3
Ben-Malik <benmaliktchamalam@gmail.com>
2022-03-04 08:47:41 +0300

    Refactored the implementtion of the media api interface

--- src/main/java/com/my/media/service/MediaManagerImpl.java
+++ src/main/java/com/my/media/service/MediaManagerImpl.java
@@ -10,9 +10,15 @@ import java.net.URL;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Comparator;
+import java.util.HashSet;
 import java.util.List;
-import java.util.concurrent.CompletableFuture;
+import java.util.Set;
+import java.util.concurrent.Callable;
 import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import java.util.concurrent.TimeUnit;
 
 import com.my.media.enums.MediaType;
 import com.my.media.model.Media;
@@ -40,6 +46,7 @@ public class MediaManagerImpl implements MediaManager {
     private static final String GOOGLE_BOOKS_API = "https://www.googleapis.com/books/v1/volumes?q=";
     private static final int GOOGLE_BOOK_API_MAXIMUM_RESULT_VALUE = 40;
     private static final int DEFAULT_START_INDEX = 0;
+    private static final int MAXIMUM_NUMBER_OF_MINUTE_TO_WAIT = 1;
 
     @Value("${upstreamLimit}")
     private int upstreamLimit;
@@ -49,23 +56,38 @@ public class MediaManagerImpl implements MediaManager {
 
         List<Media> allMedia = new ArrayList<>();
 
-        CompletableFuture<Void> books = CompletableFuture.runAsync(() -> {
-            allMedia.addAll(findBooksBy(input, false));
+        ExecutorService executorService = Executors.newFixedThreadPool(2);
+        Set<Callable<List<Media>>> callables = new HashSet<Callable<List<Media>>>();
+        
+        callables.add(new Callable<List<Media>>() {
+            public List<Media> call() throws Exception {
+                return findAlbumsBy(input, false);
+            }
+        });
+        callables.add(new Callable<List<Media>>() {
+            public List<Media> call() throws Exception {
+                return findBooksBy(input, false);
+            }
         });
 
-        CompletableFuture<Void> albums = CompletableFuture.runAsync(() -> {
-            allMedia.addAll(findAlbumsBy(input, false));
-        });
-
-        CompletableFuture<Void> apiFuture = CompletableFuture.allOf(books, albums);
         try {
-            apiFuture.get();
-        } catch (InterruptedException | ExecutionException e) {
-            e.printStackTrace();
-        }
+            List<Future<List<Media>>> futures = executorService.invokeAll(callables);
+            for (Future<List<Media>> future: futures) {
+                allMedia.addAll(future.get());
+            }
+            executorService.shutdown();
 
+            if (!executorService.awaitTermination(MAXIMUM_NUMBER_OF_MINUTE_TO_WAIT, TimeUnit.MINUTES)) {
+                executorService.shutdownNow();
+            } 
+        } catch (InterruptedException | ExecutionException e1) {
+            e1.printStackTrace();
+        }
+        
         allMedia.sort(Comparator.comparing((Media::getTitle)));
-        System.out.println("All media size: " + allMedia.size());
+
+        System.out.println("Total Books: " + allMedia.size());
+        allMedia.forEach(e -> System.out.println(e.toString()));
         return allMedia;
     }
 
@@ -93,6 +115,7 @@ public class MediaManagerImpl implements MediaManager {
         if (sortOutput) {
             searchResult.sort(Comparator.comparing((Media::getTitle)));
         }
+        System.out.println("Album Size: " + searchResult.size());
         return searchResult;
     }
 
@@ -101,79 +124,103 @@ public class MediaManagerImpl implements MediaManager {
 
         List<Media> searchResult = new ArrayList<>();
 
-        for (Boundary boundary: getRequestBoundaries(upstreamLimit)) {
-            
-            System.out.println(boundary.toString());
-            
-            String query = prepareQuery(input, MediaType.BOOK, boundary.getStart(), boundary.getLength());
-            System.out.println("Query: " + query);
-            try {
-                URL url = new URL(query);
-                HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-                connection.setRequestMethod("GET");
-    
-                int responseCode = connection.getResponseCode();
-                System.out.println("Response Code: " + responseCode);
-    
-                BufferedReader in = new BufferedReader(
-                        new InputStreamReader(connection.getInputStream()));
-    
-                String inputLine;
-                StringBuffer response = new StringBuffer();
-    
-                while ((inputLine = in.readLine()) != null) {
-                    response.append(inputLine);
+        List<Boundary> requestBoundaries = getRequestBoundaries(upstreamLimit);
+
+        ExecutorService executorService = Executors.newFixedThreadPool(requestBoundaries.size());
+
+        Set<Callable<List<Media>>> callables = new HashSet<Callable<List<Media>>>();
+        for (Boundary boundary : requestBoundaries) {
+            callables.add(new Callable<List<Media>>() {
+                public List<Media> call() throws Exception {
+                    String query = prepareQuery(input, MediaType.BOOK, boundary.getStart(), boundary.getLength());
+                    System.out.println("Query: " + query);
+                    return executeBookQuery(query);
                 }
-                in.close();
-                String responseBooks = response.toString();
-                JSONObject responseAsJSONObject = new JSONObject(responseBooks);
-    
-                // In case the first output happens to have no items available.
-                if (Integer.parseInt(responseAsJSONObject.get("totalItems").toString()) == 0) {
-                    break;
-                }
-                JSONArray responseAJsonArray = responseAsJSONObject.getJSONArray("items");
-    
-                for (Object o : responseAJsonArray) {
-                    List<String> authors = new ArrayList<>();
-    
-                    JSONObject currentObject = (JSONObject) o;
-                    JSONObject volumeInfoObject = currentObject.getJSONObject("volumeInfo");
-                    String title = volumeInfoObject.getString("title");
-                    if (!volumeInfoObject.isNull("authors")) {
-                        JSONArray authorsArray = volumeInfoObject.getJSONArray("authors");
-    
-                        if (authorsArray != null && !authorsArray.isEmpty()) {
-                            for (Object author : authorsArray) {
-                                authors.add(author.toString());
-                            }
-                        }
-                    }
-                    Media currentMedia = new Media(title, authors, MediaType.BOOK);
-                    searchResult.add(currentMedia);
-                }
-                if (sortOutput) {
-                    searchResult.sort(Comparator.comparing((Media::getTitle)));
-                }
-            } catch (MalformedURLException e22) {
-                e22.printStackTrace();
-            } catch (ProtocolException e1) {
-                e1.printStackTrace();
-            } catch (IOException e2) {
-                e2.printStackTrace();
-            }
+            });
         }
-       
-        System.out.println("Total Books: " + searchResult.size());
-        searchResult.forEach(e -> System.out.println(e.toString()));
+
+        try {
+            List<Future<List<Media>>> futures = executorService.invokeAll(callables);
+            for (Future<List<Media>> future: futures) {
+                searchResult.addAll(future.get());
+            }
+            executorService.shutdown();
+        } catch (InterruptedException | ExecutionException e) {
+            e.printStackTrace();
+        }
+    
+        if (sortOutput) {
+            searchResult.sort(Comparator.comparing((Media::getTitle)));
+        }
+        System.out.println("Book Size: " + searchResult.size());
+
         return searchResult;
     }
 
-    private List<Media> executeQuery(String query) {
+    /**
+     * A helper method to execture a given Query for fetching the books.
+     * @param query The query to be exectured.
+     * @return a list of {@linkplain Media}s.
+     */
+    private List<Media> executeBookQuery(String query) {
         List<Media> queryResult = new ArrayList<>();
 
+        try {
+            URL url = new URL(query);
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setRequestMethod("GET");
+
+            int responseCode = connection.getResponseCode();
+            System.out.println("Response Code: " + responseCode);
+
+            BufferedReader in = new BufferedReader(
+                    new InputStreamReader(connection.getInputStream()));
+
+            String inputLine;
+            StringBuffer response = new StringBuffer();
+
+            while ((inputLine = in.readLine()) != null) {
+                response.append(inputLine);
+            }
+            in.close();
+            String responseBooks = response.toString();
+            JSONObject responseAsJSONObject = new JSONObject(responseBooks);
+
+            // In case the first output happens to have no items available.
+            if (Integer.parseInt(responseAsJSONObject.get("totalItems").toString()) == 0) {
+                return queryResult;
+            }
+            JSONArray responseAJsonArray = responseAsJSONObject.getJSONArray("items");
+
+            for (Object o : responseAJsonArray) {
+                List<String> authors = new ArrayList<>();
+
+                JSONObject currentObject = (JSONObject) o;
+                JSONObject volumeInfoObject = currentObject.getJSONObject("volumeInfo");
+                String title = volumeInfoObject.getString("title");
+                if (!volumeInfoObject.isNull("authors")) {
+                    JSONArray authorsArray = volumeInfoObject.getJSONArray("authors");
+
+                    if (authorsArray != null && !authorsArray.isEmpty()) {
+                        for (Object author : authorsArray) {
+                            authors.add(author.toString());
+                        }
+                    }
+                }
+                Media currentMedia = new Media(title, authors, MediaType.BOOK);
+                queryResult.add(currentMedia);
+            }
+
+        } catch (MalformedURLException e22) {
+            e22.printStackTrace();
+        } catch (ProtocolException e1) {
+            e1.printStackTrace();
+        } catch (IOException e2) {
+            e2.printStackTrace();
+        }
         return queryResult;
     }
+
     /**
      * Prepares the query for the media given a text input and a media type.
      * 
@@ -189,7 +236,7 @@ public class MediaManagerImpl implements MediaManager {
             sb.append(ITUNES_URL + input + "&limit=" + limit);
         } else {
             sb.append(GOOGLE_BOOKS_API + input + "&maxResults="
-                    + (limit > GOOGLE_BOOK_API_MAXIMUM_RESULT_VALUE ? GOOGLE_BOOK_API_MAXIMUM_RESULT_VALUE : limit) 
+                    + (limit > GOOGLE_BOOK_API_MAXIMUM_RESULT_VALUE ? GOOGLE_BOOK_API_MAXIMUM_RESULT_VALUE : limit)
                     + "&startIndex=" + startIndex);
         }
 
@@ -197,20 +244,23 @@ public class MediaManagerImpl implements MediaManager {
     }
 
     /***
-     * TODO JAVADOC
+     *   <div> A private class to encapsulate the data or boundaries used while dividing the queries. 
+     *   That is, when the upstream limit is more than the GOOGLE_BOOK_API_MAXIMUM_RESULT_VALUE, 
+     *   we divide things into boundaries, and then flush the queries using concurrency.
+     * </div>
      */
     @Data
     @ToString
     @AllArgsConstructor
-    class Boundary {
+    private class Boundary {
         private int start;
         private int length;
     }
 
     /***
-     * 
-     * @param limit
-     * @return
+     * Given an integer value as limit, chunks it into a lit of {@linkplain Boundary} having the same lenght. 
+     * @param limit The limit to be used.
+     * @return a list of {@linkplain Boundary}
      */
     private List<Boundary> getRequestBoundaries(int limit) {
         List<Boundary> output = new ArrayList<>();
@@ -218,7 +268,7 @@ public class MediaManagerImpl implements MediaManager {
         int currentStart = DEFAULT_START_INDEX;
         int subrangeLength = GOOGLE_BOOK_API_MAXIMUM_RESULT_VALUE;
 
-        for (int i = 0;  i < numberOfRange; i++) {
+        for (int i = 0; i < numberOfRange; i++) {
             output.add(new Boundary(currentStart, subrangeLength));
             currentStart += subrangeLength;
         }
